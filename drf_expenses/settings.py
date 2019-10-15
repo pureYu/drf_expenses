c80f081422326ae4758c53e0a7388e6371dff21c
pureYu <yuliya.s.belovol@gmail.com>
2019-10-15 17:07:58 +0300

    react front-end implementation started

--- drf_expenses/settings.py
+++ drf_expenses/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
+
     'rest_framework.authtoken',
     'rest_auth',
     'django.contrib.sites',
@@ -61,6 +63,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:8080',
+)
+
--- expenses/views.py
+++ expenses/views.py
@@ -10,16 +10,26 @@ User = settings.AUTH_USER_MODEL
 from .models import Expense
 from .serializers import ExpenseSerializer
 from users.models import CustomUser
+from users.permissions import *
 
 from .filters import *
 from django_filters import rest_framework as filters
 
 
+permission_groups = {
+    'post': ['regular_user', 'user_manager', 'admin'],  # can POST
+    'put': ['regular_user', 'user_manager', 'admin'],  # can PATCH
+    # 'get': ['regular_user', 'user_manager', 'admin', '_Public'], # retrieve can be accessed without credentials (GET 'site.com/api/foo/1')
+    'get': ['regular_user', 'user_manager', 'admin'], # retrieve can be accessed without credentials (GET 'site.com/api/foo/1')
+    'delete': ['regular_user', 'user_manager', 'admin'], # retrieve can be accessed without credentials (GET 'site.com/api/foo/1')
+}
+
 class ExpenseView(ListCreateAPIView):
     """
     View for listing users' expenses and CRUD their expenses
     """
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = [IsAuthenticated, HasGroupPermission]
+    # permission_groups = permission_groups
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
     filter_backends = (filters.DjangoFilterBackend,)
@@ -28,8 +38,8 @@ class ExpenseView(ListCreateAPIView):
     def get_queryset(self):
         # queryset = Expense.objects.filter(author=self.request.user)
         filter = {}
-        if IsAuthenticated and self.request.user.id != 1:
-            filter['author'] = self.request.user
+        # if IsAuthenticated and self.request.user.id != 1:  # Cannot cast AnonymousUser to int. Are you trying to use it in place of User?
+        #     filter['author'] = self.request.user
         queryset = Expense.objects.filter(**filter)
         # return queryset.order_by('-date_spent', '-id')
         return queryset.order_by('-id')
@@ -40,6 +50,7 @@ class ExpenseView(ListCreateAPIView):
 
 
 class SingleExpenseView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = [IsAuthenticated, HasGroupPermission]
+    # permission_groups = permission_groups
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
